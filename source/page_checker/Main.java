2c5f7576e22ea8a6653b698d4b43e01e072f1309
piaand <pia.m.andersin@gmail.com>
2020-06-29 19:43:34 +0300

    added time checker for main

--- source/page_checker/Main.java
+++ source/page_checker/Main.java
@@ -11,16 +11,33 @@ public class Main {
                 if (args.length != 1) {
                     printInstructions();
                 } else {
+                    long cycle = parseCycle(args[0]);
+                    System.out.println("Started the program repeting check every "+ args[0] + " hours.");
                     Timer timer = new Timer();
-                    timer.schedule(new PageChecker(), 0, 5000);
+                    timer.schedule(new PageChecker(), 0, cycle);
                 }
             } catch (Exception e) {
                 throw e;
             }
         }
         
+        public static long parseCycle(String hours) throws Exception {
+            try {
+                long hours_long = Long.parseLong(hours);
+                long nanos = hours_long * 60 * 60 * 1000;
+                if (nanos < 1) {
+                    throw new java.lang.RuntimeException("Error: 0 or smaller cannot be repeated. Give argument 1 or higher");
+                } else {
+                    return (nanos);
+                }
+            } catch (Exception e){
+                System.out.println("Please give the argument in hours the program should be repeated.");
+                throw e;
+            }
+        }
+        
         public static void printInstructions() {
-            System.out.println("You need more/less arguments");
+            System.out.println("Start program by giving it only one argument. That argument represents the amount of hours the program should be repeated. Amount can be minimun of 1.");
         }
         
         
